621914a1cb1896de7272685dd3d4666c624cea48
Jac22 <jacopo.prudentino@gmail.com>
2023-02-04 18:28:58 +0100

    first commit

---
+++ database/migrations/2023_02_04_171001_create_smth_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('smth', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64)->unique();
+            $table->date('a_date');
+            $table->integer('a_number');
+            $table->decimal('price', $precision=6, $scale=2)->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('smth');
+    }
+};
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main_layout')
+
+@section('content')
+    <h1>Hello</h1>
+@endsection
