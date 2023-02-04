7a77c43c438f13f798b0731a824eeade0c671ed6
Jac22 <jacopo.prudentino@gmail.com>
2023-02-04 22:08:05 +0100

    Stampate le cose

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Thing;
 
 class MainController extends Controller
 {
     public function home(){
-        return view('pages.home');
+        $things = Thing::all();
+        return view('pages.home', compact('things'));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.main_layout')
 
 @section('content')
-    <h1>Hello</h1>
+    <ul>
+        @foreach ($things as $thing)
+        <li>
+            <a href="#">{{$thing->name}}  {{$thing->a_date}}  {{$thing->a_number}}  {{$thing->price}}</a>    
+        </li>
+        @endforeach
+    </ul>
 @endsection
